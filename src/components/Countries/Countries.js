2c1edc9205c3f0d45a1c9f9e579a1875d08206a3
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-03-31 20:10:47 +0600

    post detais added

---
+++ src/components/Countries/Countries.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Countries = () => {
+  return (
+    <div>
+      <h3>Welcome to my world</h3>
+    </div>
+  );
+};
+
+export default Countries;
--- src/components/PostDetail/PostDetail.js
+++ src/components/PostDetail/PostDetail.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
+import "./PostDetail.css";
 const PostDetail = () => {
   const { postId } = useParams();
+  const [post, setPost] = useState({});
+  useEffect(() => {
+    const url = `https://jsonplaceholder.typicode.com/posts/${postId}`;
+    console.log(url);
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setPost(data));
+  }, [postId]);
   return (
     <div>
       <h4>This is Post detail for: {postId}</h4>
+      <div className="single-post">
+        <p>User ID: {post.userId}</p>
+        <p>Title: {post.title}</p>
+        <p className="body">{post.body}</p>
+      </div>
     </div>
   );
 };
